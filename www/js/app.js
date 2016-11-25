ee461def9965e439ae593e47f89dd65b7cc75aea
Boyd <wolff_cristy@yahoo.com>
2016-11-25 21:28:46 +0200

    Added menu

--- www/js/app.js
+++ www/js/app.js
@@ -5,52 +5,52 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 var app = angular.module('starter', ['ionic', 'starter.controllers'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-  
-  
-  // .state('app.single', {
-  //   url: '/playlists/:playlistId',
-  //   views: {
-  //     'menuContent': {
-  //       templateUrl: 'templates/playlist.html',
-  //       controller: 'PlaylistCtrl'
-  //     }
-  //   }
-
-  .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-    .state('app.home', {
-      url: '/',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/home.html',
-          controller: 'HomeController'
-        }
-      }
-
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/');
-});
+	
+	.run(function ($ionicPlatform) {
+		$ionicPlatform.ready(function () {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			if (window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+				
+			}
+			if (window.StatusBar) {
+				// org.apache.cordova.statusbar required
+				StatusBar.styleDefault();
+			}
+		});
+	})
+	
+	
+	// .state('app.single', {
+	//   url: '/playlists/:playlistId',
+	//   views: {
+	//     'menuContent': {
+	//       templateUrl: 'templates/playlist.html',
+	//       controller: 'PlaylistCtrl'
+	//     }
+	//   }
+	
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			
+			.state('app', {
+				url: '/app',
+				abstract: true,
+				templateUrl: 'templates/menu.html',
+				controller: 'AppCtrl'
+			})
+			.state('app.home', {
+				url: '/',
+				views: {
+					'menuContent': {
+						templateUrl: 'templates/home.html',
+						controller: 'HomeController'
+					}
+				}
+				
+			});
+		// if none of the above states are matched, use this as the fallback
+		$urlRouterProvider.otherwise('/app/');
+	});
