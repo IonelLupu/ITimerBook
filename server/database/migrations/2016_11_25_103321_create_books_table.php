e715e0bc5954ad0a3d5355c715ee64cdd8ec8cf0
Boyd <wolff_cristy@yahoo.com>
2016-12-05 22:31:23 +0200

    changed the books table migration

--- server/database/migrations/2016_11_25_103321_create_books_table.php
+++ server/database/migrations/2016_11_25_103321_create_books_table.php
@@ -18,7 +18,7 @@ class CreateBooksTable extends Migration
             $table->string('title');
             $table->integer('pages');
             $table->string('author')->nullable();
-            $table->text('descripton')->nullable();
+            $table->text('description')->nullable();
             $table->integer('user_id')->unsigned();
 
             $table->foreign('user_id')->references('id')->on('users');
