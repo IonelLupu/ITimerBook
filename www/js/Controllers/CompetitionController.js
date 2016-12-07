365be301366d787d0f8c9a1787a05114fe300c0d
LarisaDumitru <larisa.dumitru2702@yahoo.com>
2016-12-07 10:52:52 +0200

    pagina  inregistrare cu parola ascunsa si adresa

--- www/js/Controllers/CompetitionController.js
+++ www/js/Controllers/CompetitionController.js
@@ -3,7 +3,17 @@ app.controller('CompetitionController', function($scope, $stateParams, Server) {
     $scope.competition = {
         starts_at :"19.12.16",
         ends_at :"25.12.16",
-        points : "24.5.16",
+        points : "200",
+        prize : {
+            title : "Carte 1",
+            author: "Ionel Florescu",
+        },
+
+    },
+    {
+        starts_at :"26.12.16",
+        ends_at :"1.1.16",
+        points : "400",
         prize : {
             title : "Carte 1",
             author: "Ionel Florescu",
--- www/js/Controllers/RegisterController.js
+++ www/js/Controllers/RegisterController.js
@@ -1,7 +1,37 @@
 
 app.controller('RegisterController', function($scope, $state, $stateParams, toastr, Server) {
 
-
+    $scope.categories =
+        {
+            name:"Fantastic"
+        },
+        {
+            name:"Mister"
+        },
+        {
+            name:"Fictiune"
+        },
+        {
+            name:"Realism"
+        },
+        {
+            name:"Comedie"
+        },
+        {
+            name:"Horor"
+        },
+        {
+            name:"Romanta"
+        },
+        {
+            name:"Satir"
+        },
+        {
+            name:"Tragedie"
+        },
+        {
+            name:"Folclor"
+        },
     $scope.registerUser = function(data){
 
         Server.post('register',data).then(function(){
