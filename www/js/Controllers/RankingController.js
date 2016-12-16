60d2ac5096019fba0259df7e72a8ae65100fdd83
LarisaDumitru <larisa.dumitru2702@yahoo.com>
2016-12-16 20:14:38 +0200

    Pagina clasament

---
+++ www/js/Controllers/RankingController.js
@@ -0,0 +1,26 @@
+app.controller('RankingController', function($scope, $state, $stateParams, toastr, Server) {
+    Server.get("ranking").success(function(resp){
+        $scope.users = resp;
+    })
+
+    $scope.users = [
+
+        {
+           firstName:"Gigi",
+           lastName:"Duta",
+            points:1200
+        },
+
+        {
+            firstName:"Lili",
+            lastName:"Puta",
+            points:1900
+        },
+
+        {
+            firstName:"Piki",
+            lastName:"Muta",
+            points:100
+        },
+    ]
+});
