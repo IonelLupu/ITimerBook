459c7f7cf6733aab877f13c495252b1d42a57295
Boyd <wolff_cristy@yahoo.com>
2017-02-07 15:23:55 +0200

    Fixed bugs

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -22,13 +22,20 @@ class User extends Model implements
 	use Authenticatable, CanResetPassword;
 	use Notifiable, EntrustUserTrait;
 
+
+	public $timestamps = false;
+
+	protected $appends = ['level'];
+
+	protected $with = ['categories'];
+
 	/**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
 	 */
 	protected $fillable = [
-		'name', 'email', 'password',
+		'firstName', 'lastName', 'email', 'password', 'gender','address','minutesForReading',
 	];
 
 	/**
@@ -39,4 +46,35 @@ class User extends Model implements
 	protected $hidden = [
 		'password', 'remember_token',
 	];
+
+	public function categories()
+	{
+		return $this->belongsToMany(Category::class, 'users_categories');
+	}
+
+	public function books()
+	{
+		return $this->hasMany(Book::class);
+	}
+
+	public function notFinishedBooks()
+	{
+		return $this->books()->where('finished', 0)->get();
+	}
+
+
+	public function getLevelAttribute()
+	{
+		if ($this->points <= 2500)
+			return 'Incepator';
+
+		if ($this->points > 2500 && $this->points <= 6000)
+			return 'Mediu';
+
+		if ($this->points > 6000 && $this->points <= 10000)
+			return 'Avansat';
+
+		if ($this->points > 10000)
+			return 'Expert';
+	}
 }
